8b0b0ca22f1c615dcd46b81d83ea0bae0e88dadb
Gian Giovani <wejick@gmail.com>
2011-07-15 15:48:29 +0700

    adding 2 class and two header

---
+++ lab/animesprite.h
@@ -0,0 +1,15 @@
+/* 
+copyright	: Gian Giovani 2011
+Contact		: wejick@gmail.com
+Blog		: wejick.wordpress.com
+<<license>>
+*/
+
+#ifndef ANIMESPRITE_H
+#define ANIMESPRITE_H
+
+#include <QPainter>
+#include <QPixmap>
+
+
+#endif // ANIMESPRITE_H
